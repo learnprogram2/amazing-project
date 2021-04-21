f887a2119b55df6cc2baa3a0a5a32d49c7b96803
csdaf <fwe>
2021-04-21 21:12:26 +0800

    registry-client: 1. atomic+version部分替代synchronized加锁: 对于不重要的更新可以cas允许失败, 不用一直等锁

--- amazing-client/src/main/java/cn/gasin/client/Registry.java
+++ amazing-client/src/main/java/cn/gasin/client/Registry.java
@@ -10,6 +10,7 @@ import lombok.extern.log4j.Log4j2;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static cn.gasin.client.config.ClientConfig.REGISTRY_FETCH_INTERVAL;
 
@@ -21,10 +22,11 @@ public class Registry extends Thread {
     /**
      * 注册表: Map<serviceName: Map<instanceId, RegistryInfo>>
      */
-    private Map<String, Map<String, InstanceInfo>> registry;
+    private volatile Map<String, Map<String, InstanceInfo>> registry;
     // 优化一: 把registry包装到一个obj里面, 用AtomicReference, 这个优化不做, 太麻烦, 而且并发量不高.
     // 优化二: 使用AtomicStampedReference来避免ABA问题, 这个就有点扯了.
     // 问题是: 就算是用了atomicReference也不能避免这个map的并发操作不出问题啊.
+    private AtomicInteger registryVersion = new AtomicInteger(0); // 对registry的修改, 使用version做好ABA的防护.
 
     private final RegisterClientWorker registerClientWorker;
     private final HttpClient httpClient;
@@ -42,25 +44,32 @@ public class Registry extends Thread {
 
         // 初始化拉全部注册表
         while (!initialize) {
-            registry = httpClient.fetchRegistry();
-            initialize = true;
+            int expectedVersion = registryVersion.get();
+            Map<String, Map<String, InstanceInfo>> serverRegistry = httpClient.fetchRegistry();
+            if (registryVersion.compareAndSet(expectedVersion, expectedVersion + 1)) {
+                initialize = true;
+                registry = serverRegistry;
+            }
         }
 
         // 每30s拉取注册表
         while (registerClientWorker.running()) {
             try {
-                synchronized (registry) {
-                    // 直接拿来替换, 暂时不过滤啊做别的处理.
-                    QueryRegistryResponse deltaResponse = httpClient.fetchDeltaRegistry();
-                    mergeIntoRegistry(deltaResponse.getDeltaInstanceInfoList());
-                    log.info("fetch registry success, contains [{}] service.", registry.size());
-                    // 优化: 校验 merge的对不对
-                    int clientInstanceCount = 0;
-                    for (Map<String, InstanceInfo> value : registry.values()) {
-                        clientInstanceCount += value.size();
-                    }
-                    if (deltaResponse.getInstanceCount() != clientInstanceCount) {
-                        registry = httpClient.fetchRegistry();
+                int expectedVersion = registryVersion.get();
+                // 直接拿来替换, 暂时不过滤啊做别的处理.
+                QueryRegistryResponse deltaResponse = httpClient.fetchDeltaRegistry();
+                if (registryVersion.compareAndSet(expectedVersion, expectedVersion + 1)) {
+                    synchronized (Registry.class) {
+                        mergeIntoRegistry(deltaResponse.getDeltaInstanceInfoList());
+                        log.info("fetch registry success, contains [{}] service.", registry.size());
+                        // 优化: 校验 merge的对不对
+                        int clientInstanceCount = 0;
+                        for (Map<String, InstanceInfo> value : registry.values()) {
+                            clientInstanceCount += value.size();
+                        }
+                        if (deltaResponse.getInstanceCount() != clientInstanceCount) {
+                            registry = httpClient.fetchRegistry();
+                        }
                     }
                 }
             } catch (Exception e) {
@@ -75,6 +84,7 @@ public class Registry extends Thread {
                 }
             }
         }
+
     }
 
     private void mergeIntoRegistry(List<InstanceInfoChangedHolder> fetchDeltaRegistry) {
